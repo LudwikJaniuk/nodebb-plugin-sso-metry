d86d56910cafad5ed9e2ac05778038f11d89b8ac
Ludvig Janiuk <ludvig.janiuk@gmail.com>
2018-06-04 21:06:56 +0200

    prettier code

--- library.js
+++ library.js
@@ -1,40 +1,40 @@
-(function(module) {
-	"use strict";
+(function (module) {
+  "use strict";
 
-	/*
-		Welcome to the Metry SSO plugin! If you're inspecting this code, you're probably looking to
-		hook up NodeBB with your existing OAuth endpoint.
+  /*
+    Welcome to the Metry SSO plugin! If you're inspecting this code, you're probably looking to
+    hook up NodeBB with your existing OAuth endpoint.
 
-		Step 1: Fill in the "constants" section below with the requisite informaton. Either the "oauth"
-				or "oauth2" section needs to be filled, depending on what you set "type" to.
+    Step 1: Fill in the "constants" section below with the requisite informaton. Either the "oauth"
+        or "oauth2" section needs to be filled, depending on what you set "type" to.
 
-		Step 2: Give it a whirl. If you see the congrats message, you're doing well so far!
+    Step 2: Give it a whirl. If you see the congrats message, you're doing well so far!
 
-		Step 3: Customise the `parseUserReturn` method to normalise your user route's data return into
-				a format accepted by NodeBB. Instructions are provided there. (Line 146)
+    Step 3: Customise the `parseUserReturn` method to normalise your user route's data return into
+        a format accepted by NodeBB. Instructions are provided there. (Line 146)
 
-		Step 4: If all goes well, you'll be able to login/register via your OAuth endpoint credentials.
-	*/
+    Step 4: If all goes well, you'll be able to login/register via your OAuth endpoint credentials.
+  */
 
-	var User = module.parent.require('./user'),
-		Groups = module.parent.require('./groups'),
-		meta = module.parent.require('./meta'),
-		db = module.parent.require('../src/database'),
-		passport = module.parent.require('passport'),
-		fs = module.parent.require('fs'),
-		path = module.parent.require('path'),
-		nconf = module.parent.require('nconf'),
-		winston = module.parent.require('winston'),
-		async = module.parent.require('async');
+  var User = module.parent.require('./user'),
+    Groups = module.parent.require('./groups'),
+    meta = module.parent.require('./meta'),
+    db = module.parent.require('../src/database'),
+    passport = module.parent.require('passport'),
+    fs = module.parent.require('fs'),
+    path = module.parent.require('path'),
+    nconf = module.parent.require('nconf'),
+    winston = module.parent.require('winston'),
+    async = module.parent.require('async');
 
-	var authenticationController = module.parent.require('./controllers/authentication');
+  var authenticationController = module.parent.require('./controllers/authentication');
 
-	/**
-	 * REMEMBER
-	 *   Never save your OAuth Key/Secret or OAuth2 ID/Secret pair in code! It could be published and leaked accidentally.
-	 *   Save it into your config.json file instead:
-	 *
-	 *   {
+  /**
+   * REMEMBER
+   *   Never save your OAuth Key/Secret or OAuth2 ID/Secret pair in code! It could be published and leaked accidentally.
+   *   Save it into your config.json file instead:
+   *
+   *   {
 	 *     ...
 	 *     "oauth": {
 	 *       "id": "someoauthid",
@@ -42,237 +42,243 @@
 	 *     }
 	 *     ...
 	 *   }
-	 *
-	 *   ... or use environment variables instead:
-	 *
-	 *   `OAUTH__ID=someoauthid OAUTH__SECRET=youroauthsecret node app.js`
-	 */
+   *
+   *   ... or use environment variables instead:
+   *
+   *   `OAUTH__ID=someoauthid OAUTH__SECRET=youroauthsecret node app.js`
+   */
 
-	var constants = Object.freeze({
-			type: 'oauth2',	// Either 'oauth' or 'oauth2'
-			name: 'metry',	// Something unique to your OAuth provider in lowercase, like "github", or "nodebb"
-			oauth: {
-				requestTokenURL: '',
-				accessTokenURL: '',
-				userAuthorizationURL: '',
-				consumerKey: nconf.get('oauth:key'),	// don't change this line
-				consumerSecret: nconf.get('oauth:secret'),	// don't change this line
-			},
-			oauth2: {
-				authorizationURL: 'https://app.metry.io/oauth/authorize',
-				tokenURL: 'https://app.metry.io/oauth/token',
-				clientID: nconf.get('oauth:id'),	// don't change this line
-				clientSecret: nconf.get('oauth:secret'),	// don't change this line
-			},
+  var constants = Object.freeze({
+      type: 'oauth2',	// Either 'oauth' or 'oauth2'
+      name: 'metry',	// Something unique to your OAuth provider in lowercase, like "github", or "nodebb"
+      oauth: {
+        requestTokenURL: '',
+        accessTokenURL: '',
+        userAuthorizationURL: '',
+        consumerKey: nconf.get('oauth:key'),	// don't change this line
+        consumerSecret: nconf.get('oauth:secret'),	// don't change this line
+      },
+      oauth2: {
+        authorizationURL: 'https://app.metry.io/oauth/authorize',
+        tokenURL: 'https://app.metry.io/oauth/token',
+        clientID: nconf.get('oauth:id'),	// don't change this line
+        clientSecret: nconf.get('oauth:secret'),	// don't change this line
+      },
       userRoute: 'https://app.metry.io/api/v2/accounts/me',	// This is the address to your app's "user profile" API endpoint (expects JSON)
       collaboratorRoute: 'https://app.metry.io/api/v2/accounts/me/authenticated_collaborator',
       scope: nconf.get('oauth:scope') || 'basic'
-		}),
-		configOk = false,
+    }),
+    configOk = false,
     OAuth = {}, passportOAuth, opts;
 
-	if (!constants.name) {
-		winston.error('[sso-metry] Please specify a name for your OAuth provider (library.js:32)');
-	} else if (!constants.type || (constants.type !== 'oauth' && constants.type !== 'oauth2')) {
-		winston.error('[sso-metry] Please specify an OAuth strategy to utilise (library.js:31)');
-	} else if (!constants.userRoute) {
-		winston.error('[sso-metry] User Route required (library.js:31)');
-	} else {
-		configOk = true;
-	}
+  if (!constants.name) {
+    winston.error('[sso-metry] Please specify a name for your OAuth provider (library.js:32)');
+  } else if (!constants.type || (constants.type !== 'oauth' && constants.type !== 'oauth2')) {
+    winston.error('[sso-metry] Please specify an OAuth strategy to utilise (library.js:31)');
+  } else if (!constants.userRoute) {
+    winston.error('[sso-metry] User Route required (library.js:31)');
+  } else {
+    configOk = true;
+  }
 
-	OAuth.getStrategy = function(strategies, callback) {
-		if (configOk) {
-			passportOAuth = require('passport-oauth')[constants.type === 'oauth' ? 'OAuthStrategy' : 'OAuth2Strategy'];
+  OAuth.getStrategy = function (strategies, callback) {
+    if (configOk) {
+      passportOAuth = require('passport-oauth')[constants.type === 'oauth' ? 'OAuthStrategy' : 'OAuth2Strategy'];
 
-			if (constants.type === 'oauth') {
-				// OAuth options
-				opts = constants.oauth;
-				opts.callbackURL = nconf.get('url') + '/auth/' + constants.name + '/callback';
+      if (constants.type === 'oauth') {
+        // OAuth options
+        opts = constants.oauth;
+        opts.callbackURL = nconf.get('url') + '/auth/' + constants.name + '/callback';
 
-				passportOAuth.Strategy.prototype.userProfile = function(token, secret, params, done) {
-					this._oauth.get(constants.userRoute, token, secret, function(err, body, res) {
-						if (err) { return done(new InternalOAuthError('failed to fetch user profile', err)); }
+        passportOAuth.Strategy.prototype.userProfile = function (token, secret, params, done) {
+          this._oauth.get(constants.userRoute, token, secret, function (err, body, res) {
+            if (err) {
+              return done(new InternalOAuthError('failed to fetch user profile', err));
+            }
 
-						try {
-							var json = JSON.parse(body);
-							OAuth.parseUserReturn(json, function(err, profile) {
-								if (err) return done(err);
-								profile.provider = constants.name;
+            try {
+              var json = JSON.parse(body);
+              OAuth.parseUserReturn(json, function (err, profile) {
+                if (err) return done(err);
+                profile.provider = constants.name;
 
-								done(null, profile);
-							});
-						} catch(e) {
-							done(e);
-						}
-					});
-				};
-			} else if (constants.type === 'oauth2') {
-				// OAuth 2 options
-				opts = constants.oauth2;
-				opts.callbackURL = nconf.get('url') + '/auth/' + constants.name + '/callback';
+                done(null, profile);
+              });
+            } catch (e) {
+              done(e);
+            }
+          });
+        };
+      } else if (constants.type === 'oauth2') {
+        // OAuth 2 options
+        opts = constants.oauth2;
+        opts.callbackURL = nconf.get('url') + '/auth/' + constants.name + '/callback';
 
-				passportOAuth.Strategy.prototype.userProfile = function(accessToken, done) {
+        passportOAuth.Strategy.prototype.userProfile = function (accessToken, done) {
           var self = this
-					this._oauth2.get(constants.userRoute, accessToken, function(err, body, res) {
-						if (err) { return done(new InternalOAuthError('failed to fetch user profile', err)); }
+          this._oauth2.get(constants.userRoute, accessToken, function (err, body, res) {
+            if (err) {
+              return done(new InternalOAuthError('failed to fetch user profile', err));
+            }
 
-						try {
+            try {
               var json = JSON.parse(body);
 
               if (json.data.is_organization) {
-                self._oauth2.get(constants.collaboratorRoute, accessToken, function(err, body, res) {
-                  if (err) { return done(new InternalOAuthError('failed to fetch user profile', err)); }
+                self._oauth2.get(constants.collaboratorRoute, accessToken, function (err, body, res) {
+                  if (err) {
+                    return done(new InternalOAuthError('failed to fetch user profile', err));
+                  }
 
-                  OAuth.parseUserReturn(JSON.parse(body), function(err, profile) {
+                  OAuth.parseUserReturn(JSON.parse(body), function (err, profile) {
                     if (err) return done(err);
                     profile.provider = constants.name;
                     done(null, profile);
                   });
                 })
               } else {
-                OAuth.parseUserReturn(json, function(err, profile) {
+                OAuth.parseUserReturn(json, function (err, profile) {
                   if (err) return done(err);
                   profile.provider = constants.name;
                   done(null, profile);
                 });
               }
-						} catch(e) {
-							done(e);
-						}
-					});
-				};
-			}
+            } catch (e) {
+              done(e);
+            }
+          });
+        };
+      }
 
-			opts.passReqToCallback = true;
+      opts.passReqToCallback = true;
 
-			passport.use(constants.name, new passportOAuth(opts, function(req, token, secret, profile, done) {
-				OAuth.login({
-					oAuthid: profile.id,
-					handle: profile.displayName,
-					email: profile.emails[0].value,
-					isAdmin: profile.isAdmin
-				}, function(err, user) {
-					if (err) {
-						return done(err);
-					}
+      passport.use(constants.name, new passportOAuth(opts, function (req, token, secret, profile, done) {
+        OAuth.login({
+          oAuthid: profile.id,
+          handle: profile.displayName,
+          email: profile.emails[0].value,
+          isAdmin: profile.isAdmin
+        }, function (err, user) {
+          if (err) {
+            return done(err);
+          }
 
-					authenticationController.onSuccessfulLogin(req, user.uid);
-					done(null, user);
-				});
-			}));
+          authenticationController.onSuccessfulLogin(req, user.uid);
+          done(null, user);
+        });
+      }));
 
-			strategies.push({
-				name: constants.name,
-				url: '/auth/' + constants.name,
-				callbackURL: '/auth/' + constants.name + '/callback',
-				icon: 'fa-check-square',
-				scope: (constants.scope || '').split(',')
-			});
+      strategies.push({
+        name: constants.name,
+        url: '/auth/' + constants.name,
+        callbackURL: '/auth/' + constants.name + '/callback',
+        icon: 'fa-check-square',
+        scope: (constants.scope || '').split(',')
+      });
 
-			callback(null, strategies);
-		} else {
-			callback(new Error('OAuth Configuration is invalid'));
-		}
-	};
+      callback(null, strategies);
+    } else {
+      callback(new Error('OAuth Configuration is invalid'));
+    }
+  };
 
-	OAuth.parseUserReturn = function(body, callback) {
-		// Alter this section to include whatever data is necessary
-		// NodeBB *requires* the following: id, displayName, emails.
-		// Everything else is optional.
+  OAuth.parseUserReturn = function (body, callback) {
+    // Alter this section to include whatever data is necessary
+    // NodeBB *requires* the following: id, displayName, emails.
+    // Everything else is optional.
 
     var profile = {};
     var email = (body.data.account || body.data).username;
     var name = (body.data.account || body.data).name || email;
-		profile.id = body.data._id;
-		profile.displayName = name;
-		profile.emails = [{ value: email }];
+    profile.id = body.data._id;
+    profile.displayName = name;
+    profile.emails = [{value: email}];
 
-		// Do you want to automatically make somebody an admin? This line might help you do that...
-		// profile.isAdmin = data.isAdmin ? true : false;
+    // Do you want to automatically make somebody an admin? This line might help you do that...
+    // profile.isAdmin = data.isAdmin ? true : false;
 
-		callback(null, profile);
-	}
+    callback(null, profile);
+  }
 
-	OAuth.login = function(payload, callback) {
-		OAuth.getUidByOAuthid(payload.oAuthid, function(err, uid) {
-			if(err) {
-				return callback(err);
-			}
+  OAuth.login = function (payload, callback) {
+    OAuth.getUidByOAuthid(payload.oAuthid, function (err, uid) {
+      if (err) {
+        return callback(err);
+      }
 
-			if (uid !== null) {
-				// Existing User
-				callback(null, {
-					uid: uid
-				});
-			} else {
-				// New User
-				var success = function(uid) {
-					// Save provider-specific information to the user
-					User.setUserField(uid, constants.name + 'Id', payload.oAuthid);
-					db.setObjectField(constants.name + 'Id:uid', payload.oAuthid, uid);
+      if (uid !== null) {
+        // Existing User
+        callback(null, {
+          uid: uid
+        });
+      } else {
+        // New User
+        var success = function (uid) {
+          // Save provider-specific information to the user
+          User.setUserField(uid, constants.name + 'Id', payload.oAuthid);
+          db.setObjectField(constants.name + 'Id:uid', payload.oAuthid, uid);
 
-					if (payload.isAdmin) {
-						Groups.join('administrators', uid, function(err) {
-							callback(null, {
-								uid: uid
-							});
-						});
-					} else {
-						callback(null, {
-							uid: uid
-						});
-					}
-				};
+          if (payload.isAdmin) {
+            Groups.join('administrators', uid, function (err) {
+              callback(null, {
+                uid: uid
+              });
+            });
+          } else {
+            callback(null, {
+              uid: uid
+            });
+          }
+        };
 
-				User.getUidByEmail(payload.email, function(err, uid) {
-					if(err) {
-						return callback(err);
-					}
+        User.getUidByEmail(payload.email, function (err, uid) {
+          if (err) {
+            return callback(err);
+          }
 
-					if (!uid) {
-						User.create({
-							username: payload.handle,
-							email: payload.email
-						}, function(err, uid) {
-							if(err) {
-								return callback(err);
-							}
+          if (!uid) {
+            User.create({
+              username: payload.handle,
+              email: payload.email
+            }, function (err, uid) {
+              if (err) {
+                return callback(err);
+              }
 
-							success(uid);
-						});
-					} else {
-						success(uid); // Existing account -- merge
-					}
-				});
-			}
-		});
-	};
+              success(uid);
+            });
+          } else {
+            success(uid); // Existing account -- merge
+          }
+        });
+      }
+    });
+  };
 
-	OAuth.getUidByOAuthid = function(oAuthid, callback) {
-		db.getObjectField(constants.name + 'Id:uid', oAuthid, function(err, uid) {
-			if (err) {
-				return callback(err);
-			}
-			callback(null, uid);
-		});
-	};
+  OAuth.getUidByOAuthid = function (oAuthid, callback) {
+    db.getObjectField(constants.name + 'Id:uid', oAuthid, function (err, uid) {
+      if (err) {
+        return callback(err);
+      }
+      callback(null, uid);
+    });
+  };
 
-	OAuth.deleteUserData = function(data, callback) {
-		async.waterfall([
-			async.apply(User.getUserField, data.uid, constants.name + 'Id'),
-			function(oAuthIdToDelete, next) {
-				db.deleteObjectField(constants.name + 'Id:uid', oAuthIdToDelete, next);
-			}
-		], function(err) {
-			if (err) {
-				winston.error('[sso-metry] Could not remove OAuthId data for uid ' + data.uid + '. Error: ' + err);
-				return callback(err);
-			}
+  OAuth.deleteUserData = function (data, callback) {
+    async.waterfall([
+      async.apply(User.getUserField, data.uid, constants.name + 'Id'),
+      function (oAuthIdToDelete, next) {
+        db.deleteObjectField(constants.name + 'Id:uid', oAuthIdToDelete, next);
+      }
+    ], function (err) {
+      if (err) {
+        winston.error('[sso-metry] Could not remove OAuthId data for uid ' + data.uid + '. Error: ' + err);
+        return callback(err);
+      }
 
-			callback(null, data);
-		});
-	};
+      callback(null, data);
+    });
+  };
 
-	module.exports = OAuth;
+  module.exports = OAuth;
 }(module));
